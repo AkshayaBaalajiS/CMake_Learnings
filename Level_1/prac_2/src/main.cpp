2b9397510eefcd7c12129ed024bbd936e0332a8a
AkshayaBaalajiS <baalaji12akshay@gmail.com>
2025-12-03 09:53:32 +0530

    prac 2 please check read me

---
+++ Level_1/prac_2/src/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "math_utils.h"
+
+using namespace std;
+
+int main()
+{
+    cout << "CMake Level 1 — Task 2" << endl;
+    cout << "5 + 7 = " << addNumbers(5, 7) << endl;
+    return 0;
+}
