f50b2471870d36106a53a681428e854b37c2e1db
AkshayaBaalajiS <baalaji12akshay@gmail.com>
2025-12-05 07:50:02 +0530

    Read the readme

---
+++ Level_1/prac_4/src/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "math_utils.hpp"
+
+int main()
+{
+    std::cout << "Add 10 + 20 = " << add(10, 20) << "\n";
+    std::cout << "Sub 50 - 15 = " << sub(50, 15) << "\n";
+    return 0;
+}
