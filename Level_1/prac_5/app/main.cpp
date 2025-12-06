b43968ea637351ee9fea5e4938e741f0d3e49c9a
AkshayaBaalajiS <baalaji12akshay@gmail.com>
2025-12-06 21:33:55 +0530

    Check the readme

---
+++ Level_1/prac_5/app/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "math_utils.hpp"
+
+int main()
+{
+    std::cout << "Add 5 + 6 = " << add(5, 6) << "\n";
+    std::cout << "Sub 20 - 4 = " << sub(20, 4) << "\n";
+    return 0;
+}
