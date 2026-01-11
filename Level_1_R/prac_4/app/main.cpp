4c9b068be61ee4a022502a8ebf57a5de0b573e22
AkshayaBaalajiS <baalaji12akshay@gmail.com>
2026-01-11 18:40:39 +0530

    Prac 4

---
+++ Level_1_R/prac_4/app/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "math.hpp"
+
+int main()
+{
+    std::cout << "6 x 7 = " << multiply(6, 7) << "\n";
+    return 0;
+}
---
+++ Level_1_R/prac_4/lib/math.cpp
@@ -0,0 +1,6 @@
+#include "math.hpp"
+
+int multiply(int a, int b)
+{
+    return a * b;
+}
