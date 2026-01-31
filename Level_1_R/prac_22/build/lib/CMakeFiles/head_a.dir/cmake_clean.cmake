ffc78652d786083f2cc7ff285d349578a54d51a3
AkshayaBaalajiS <baalaji12akshay@gmail.com>
2026-01-31 17:36:20 +0530

    Prac 22 for Socket programming

---
+++ Level_1_R/prac_22/build/lib/CMakeFiles/head_a.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/head_a.dir/header.cpp.o"
+  "CMakeFiles/head_a.dir/header.cpp.o.d"
+  "libhead_a.a"
+  "libhead_a.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/head_a.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Level_1_R/prac_22/build/lib/CMakeFiles/head_so.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "../library/libhead_so.pdb"
+  "../library/libhead_so.so"
+  "CMakeFiles/head_so.dir/header.cpp.o"
+  "CMakeFiles/head_so.dir/header.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/head_so.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
